de7b2316f07e0e2e9a754af7e48c99cbedbfdd95
William Lopera <lopera.william@gmail.com>
2023-10-20 18:04:04 -0500

    Proyecto RN Expo Basico o simple

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
 
 export default function App() {
+  async function getUserLocationHandler() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg(
+        "Permiso para accesar la localización del dispositivo denegada"
+      );
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    console.log("Mi localización es: ",location);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Obtener la localización actual del dipositivo</Text>
+      <Button title="Localizar Móvil" onPress={getUserLocationHandler} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
